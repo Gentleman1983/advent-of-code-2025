3f166ff775ec85b6e2c4df49d3d47b1913ab2c78
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2025-12-07 11:15:42 +0100

    Solved day 7 part 1

--- aoc2025/aoc2025-kotlin/src/main/kotlin/de/havox_design/aoc2025/day07/Laboratories.kt
+++ aoc2025/aoc2025-kotlin/src/main/kotlin/de/havox_design/aoc2025/day07/Laboratories.kt
@@ -3,12 +3,38 @@ package de.havox_design.aoc2025.day07
 class Laboratories(private var filename: String) {
     private val data = getResourceAsText(filename)
 
-    fun processPart1(): Any =
-        21L
+    fun processPart1(): Any {
+        val emitter = data[0]
+            .indexOf('S')
+        val beams = mutableSetOf<Int>(emitter)
+        var sum = 0L
 
-    fun processPart2(): Any =
-        0L
+        data
+            .forEach { line ->
+            if (line.contains('^')) {
+                val splitters = mutableSetOf<Int>()
 
-    private fun getResourceAsText(path: String): List<String> =
-        this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
-}
+                for (i in line.indices)
+                    if (line[i] == '^') splitters.add(i)
+
+                val collisions = beams.intersect(splitters)
+
+                sum += collisions.size
+                beams.removeAll(collisions)
+
+                for (collision in collisions) {
+                    beams.add(collision - 1)
+                    beams.add(collision + 1)
+                }
+            }
+        }
+
+        return sum
+    }
+
+        fun processPart2(): Any =
+            0L
+
+        private fun getResourceAsText(path: String): List<String> =
+            this.javaClass.classLoader.getResourceAsStream(path)!!.bufferedReader().readLines()
+    }
