09dbad79ac68dd5e1d61874fe175566ec24eb39b
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2025-12-09 17:34:19 +0100

    Solved day 9 task 1

--- aoc2025/aoc2025-kotlin/src/main/kotlin/de/havox_design/aoc2025/day09/MovieTheater.kt
+++ aoc2025/aoc2025-kotlin/src/main/kotlin/de/havox_design/aoc2025/day09/MovieTheater.kt
@@ -1,13 +1,41 @@
 package de.havox_design.aoc2025.day09
 
+import de.havox_design.aoc.utils.kotlin.model.positions.Position2d
+import de.havox_design.aoc.utils.kotlin.model.positions.minus
+import kotlin.math.absoluteValue
+
 class MovieTheater(private var filename: String) {
     private val data = getResourceAsText(filename)
+        .parse()
 
     fun processPart1(): Any =
-        50L
+        data
+            .pairwise(data)
+            .maxOf { (a, b) ->
+                calculateArea(a, b)
+            }
 
     fun processPart2(): Any =
         0L
+
+    private fun calculateArea(a: Position2d<Int>, b: Position2d<Int>): Long {
+        val diff = a - b
+
+        return (diff.x.absoluteValue.toLong() + 1) * (diff.y.absoluteValue + 1)
+    }
+
+    private fun List<String>.parse(): List<Position2d<Int>> =
+        this
+            .map { value ->
+                Position2d(
+                    value
+                        .substringBefore(ROW_DELIMITER)
+                        .toInt(),
+                    value
+                        .substringAfter(ROW_DELIMITER)
+                        .toInt()
+                )
+            }
 
     private fun getResourceAsText(path: String): List<String> =
         this
@@ -16,4 +44,17 @@ class MovieTheater(private var filename: String) {
             .getResourceAsStream(path)!!
             .bufferedReader()
             .readLines()
+
+    companion object {
+        private val ROW_DELIMITER = ","
+    }
 }
+
+private fun <A, B> Iterable<A>.pairwise(other: Iterable<B>): List<Pair<A, B>> =
+    flatMapIndexed { i, a ->
+        other
+            .drop(i + 1)
+            .map { b ->
+                a to b
+            }
+    }
